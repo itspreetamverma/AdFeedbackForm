e40ee5b436431764b494e7c366c208831ca3e2a5
Preetam <itspreetamverma@gmail.com>
2026-01-09 18:39:08 +0530

    changes for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import feedbackRoutes from "./routes/feedbackRoutes.js";
+import dotenv from "dotenv"
+
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://tomato:tomato123@cluster0.rvhoxji.mongodb.net/AdFeedback")
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 app.use("/api/feedback", feedbackRoutes);
 
-app.listen(4000, () => console.log("Server running on 4000"));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server running on PORT", PORT));
+
